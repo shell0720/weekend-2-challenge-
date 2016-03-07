2855f23e3759875decaa9f34ad92f5c683281aba
shell0720 <shell0720@gmail.com>
2016-03-07 09:30:38 -0600

    week 2

--- server/public/scripts/app.js
+++ server/public/scripts/app.js
@@ -10,34 +10,51 @@ $(document).ready(function () {
       success: function (data) {
         console.log(data);
         kappaArray = data.kappa;
+        createCarousel();
         appendDom(kappaArray[index]);
-      }
+        $("#0").addClass("highlight");
+    }
     });
 });
 
-
-
-function appendDom(data) {
-  $('.people').empty();
-  $('.people').append('<div class= "kappa" id ="'+ index + '"></div>');
-  var $el= $('.people').children().last();
-  $el.append('<h1>' + (index+1) + '</h1>');
-  $el.append('<button class="previous">Prev</button>');
-  $el.append('<button class="next">Next</button>');
-  $el.append('<p>Name: ' + data.name + '</p>');
-  $el.append('<p>Location: ' + data.location + '</p>');
-  $el.append('<p>Spirit animal: ' + data.spirit_animal + '</p>');
-  $el.append('<p>Shoutout: ' + data.shoutout + '</p>');
-
+function createCarousel () {
+  $('.carousel').append("<div></div>");
+  var $el = $('.carousel').children().last();
+  for (var i = 0; i<kappaArray.length; i++) {
+    $el.append('<div class ="index-display " id = "'+i+'"</div> ');
+  }
 }
 
-function nextPerson(){
+function updateCarousel () {
+  for (var i = 0; i< kappaArray.length; i++) {
+    $("#"+i).removeClass("highlight");
+    if(i==index){
+    $("#"+i).addClass("highlight");
+  }
+}
+}
+
+function appendDom(data) {
+    $('.people').empty();
+    $('.people').append('<div class= "kappa" id ="'+ index + '"></div>');
+    var $el= $('.people').children().last();
+    $el.append('<button class="btn btn-info previous">Prev</button>');
+    $el.append('<button class="btn btn-info next">Next</button>');
+      //$el.append('<h1>' + (index+1) + '</h1>');
+      $el.append('<p>Name: ' + data.name + '</p>');
+      $el.append('<p>Location: ' + data.location + '</p>');
+      $el.append('<p>Spirit animal: ' + data.spirit_animal + '</p>');
+      $el.append('<p>Shoutout: ' + data.shoutout + '</p>');
+  }
+
+
+function nextPerson() {
   index++;
   if(index >kappaArray.length-1) {
     index = 0;
   }
-appendDom(kappaArray[index]);
-
+  appendDom(kappaArray[index]);
+  updateCarousel();
 }
 
 function prevPerson() {
@@ -46,4 +63,5 @@ function prevPerson() {
     index = kappaArray.length-1;
   }
   appendDom(kappaArray[index]);
+  updateCarousel();
 }
